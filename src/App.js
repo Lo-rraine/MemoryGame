066c3f59b9dd4d3c1e4650b7d1e29135359dcc3c
Lorrainem <moyolorraine98@gmail.com>
2023-05-03 19:32:50 +0200

    API authorization

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import Background from './components/Background';
 
+const base_url = 'https://api.pexels.com/v1/search?'
 function App() {
+    fetch(base_url, {
+        headers: {
+            Authorization: process.env.REACT_APP_AUTH_KEY
+        }
+    });
     return (
         <>
             <Background />
